b3bc134a34c46f1f34bdbb6594dcf47e49c95b91
Mario Gutierrez <mario.gutierrez@yuxiglobal.com>
2018-11-29 07:20:36 -0500

    get an observable with an specific property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Todo } from './todo.interface';
 
 @Component({
@@ -9,11 +10,16 @@ import { Todo } from './todo.interface';
 })
 export class AppComponent {
 	title = 'RxJs';
-	url = 'https://jsonplaceholder.typicode.com/todos/1';
-	todo: string;
+	// url = 'https://jsonplaceholder.typicode.com/todos/1';
+	url = 'https://yesno.wtf/api';
+	// todo: string;
+	// todo$: Observable<Todo>;
+	todo$: Observable<{ image }>;
 
 	constructor(private http: HttpClient) {
-		const stream = this.http.get(this.url);
-		stream.subscribe((resp: Todo) => (this.todo = resp.title));
+		this.todo$ = this.http.get<{ image }>(this.url);
+		// this.todo$ = this.http.get<Todo>(this.url);
+		// const stream = this.http.get(this.url);
+		// stream.subscribe((resp: Todo) => (this.todo = resp.title));
 	}
 }
